fc0fa794e373a0e519c264fd730d69e7fa3e6c55
pedrorico79 <pedro.rico@sptech.school>
2025-05-20 10:41:46 -0300

    insert do quiz no banco de dados OK

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -10,19 +10,16 @@ function listar(req, res) {
 }
 
 function cadastrar(req, res) {
-    var nome = req.body.nome;
     var fkUsuario = req.body.fkUsuarioServer;
     var pontuacao = req.body.pontuacaoServer;
 
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (fkUsuario == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+    if (fkUsuario == undefined) {
+        res.status(400).send("Seu fkUsuario está undefined!");
     } else if (pontuacao == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+        res.status(400).send("Seu pontuacao está undefined!");
     } else {
 
-        quizModel.cadastrar(nome, fkUsuario, pontuacao).then(function (resposta) {
+        quizModel.cadastrar(fkUsuario, pontuacao).then(function (resposta) {
             res.status(200).send("Carro criado com sucesso");
         }).catch(function (erro) {
             res.status(500).json(erro.sqlMessage);
